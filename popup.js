46eaa407df050589f3e5d83c83ddb77b640acbd9
Mike G <MikeGallouedec@truckstop.com>
2022-11-03 14:23:12 -0400

    clean up

--- popup.js
+++ popup.js
@@ -8,16 +8,3 @@
         await sendMessage('btnSortTableByOwner');
     });
 }());
-//
-// (function() {
-//     const sendMessage = (command) => {
-//         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//             chrome.tabs.sendMessage(tabs[0].id, {command}, function (result) {
-//                 console.log(result)
-//             });
-//         });
-//     }
-//
-//     document.getElementById('btnSortTableByOwner')
-//         .addEventListener('click', sendMessage.bind(null, 'btnSortTableByOwner'))
-// }());
